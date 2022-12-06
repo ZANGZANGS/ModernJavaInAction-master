e8d0c61e1b5f11b6c50b754687f2e91a2b9572d5
zangzangs <kimjangh94@gmail.com>
2022-12-06 18:27:59 +0900

    Quiz_05-6

--- src/main/java/modernjavainaction/chap05/Transaction.java
+++ src/main/java/modernjavainaction/chap05/Transaction.java
@@ -1,6 +1,7 @@
 package modernjavainaction.chap05;
 
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Transaction {
 
@@ -56,4 +57,90 @@ public class Transaction {
     return String.format("{%s, year: %d, value: %d}", trader, year, value);
   }
 
+  public static void main(String[] args) {
+
+    Trader raoul = new Trader("Raoul", "Cambridge");
+    Trader mario = new Trader("Mario", "Milan");
+    Trader alan = new Trader("Alan", "Cambridge");
+    Trader brian = new Trader("Brian", "Cambridge");
+
+
+    List<Transaction> transactions = Arrays.asList(
+            new Transaction(brian, 2011, 300)
+            , new Transaction(raoul, 2012, 1000)
+            , new Transaction(raoul, 2011, 400)
+            , new Transaction(mario, 2012, 710)
+            , new Transaction(mario, 2012, 700)
+            , new Transaction(alan, 2012, 950)
+    );
+
+    List<Transaction> q1 = transactions.stream()
+            .filter(v -> v.getYear() == 2011)
+            .sorted(Comparator.comparing(Transaction::getValue))
+            .collect(Collectors.toList());
+
+    System.out.println("q1");
+    q1.forEach(System.out::println);
+
+    List<String> q2 = transactions.stream()
+            .map(Transaction::getTrader)
+            .map(Trader::getCity)
+            .distinct()
+            .collect(Collectors.toList());
+
+    System.out.println("q2");
+    System.out.println(q2);
+
+    List<Trader> q3 = transactions.stream()
+            .map(Transaction::getTrader)
+            .filter(v -> "Cambridge".equals(v.getCity()))
+            .distinct()
+            .sorted(Comparator.comparing(Trader::getName))
+            .collect(Collectors.toList());
+
+    System.out.println("q3");
+    System.out.println(q3);
+
+
+    String q4 = transactions.stream()
+            .map(Transaction::getTrader)
+            .map(Trader::getName)
+            .distinct()
+            .sorted()
+            .collect(Collectors.joining());
+
+    System.out.println("q4");
+    System.out.println(q4);
+
+    boolean hasTraderInMilano = transactions.stream()
+            .anyMatch(v -> "Milan".equals(v.getTrader().getCity()));
+
+
+    System.out.println("q5");
+    System.out.println("Milan에 거래자가 있는가? " + hasTraderInMilano);
+
+
+    List<Integer> q6 = transactions.stream()
+            .filter(v -> "Cambridge".equals(v.getTrader().getCity()))
+            .map(Transaction::getValue)
+            .collect(Collectors.toList());
+    System.out.println("q6");
+    System.out.println(q6);
+
+
+    Optional<Integer> q7 = transactions.stream()
+            .map(Transaction::getValue)
+            .reduce(Integer::max);
+
+    System.out.println("q7");
+    System.out.println(q7.get());
+
+    Optional<Transaction> q8 = transactions.stream()
+            //.map(Transaction::getValue)
+            //.reduce(Integer::min);
+            .min(Comparator.comparing(Transaction::getValue));
+
+    System.out.println("q8");
+    System.out.println(q8.get().getValue());
+  }
 }
